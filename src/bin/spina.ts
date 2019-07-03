502a70e6b749037b0cf7d7561f078347171987df
Grzegorz Beń <spinajs@coderush.pl>
2019-07-03 04:36:51 +0200

    spine cli proper bootstrap

--- src/bin/spina.ts
+++ src/bin/spina.ts
@@ -1,3 +1,13 @@
-import { DI, FrameworkCliModule } from '../system/index';
- 
-DI.resolve(FrameworkCliModule);
+import { FrameworkCliModule, DI, Configuration, FrameworkConfiguration, FrameworkLogModule, LogModule } from "./../system";
+
+async function bootstrap() {
+
+    DI.register(FrameworkConfiguration).as(Configuration);
+    DI.register(FrameworkLogModule).as(LogModule);
+
+    await DI.resolve(Configuration);
+    await DI.resolve(LogModule);
+    await DI.resolve(FrameworkCliModule);
+}
+
+bootstrap().then(() => { });
